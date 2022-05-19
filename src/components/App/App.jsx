b18f13932209b7f922918011e007d28ce0fa7a51
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-05-19 19:05:46 +0300

    Update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,21 +4,25 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Form } from '../Form/Form';
 import { Section } from '../Section/Section';
 import { ContactsList } from '../ContactsList/ContactsList';
-import { Filter } from "../Filter/Filter";
-import { Loader } from "../Loader/Loader";
+import { Filter } from '../Filter/Filter';
+import { Loader } from '../Loader/Loader';
 
-import {useFetchContactsQuery } from "../../redux/contactsRTKQ";
+import { useFetchContactsQuery } from '../../redux/contactsRTKQ';
 
 export function App() {
-    const { data, isLoading } = useFetchContactsQuery();
+  const { data, isLoading } = useFetchContactsQuery();
   return (
     <Section title="Phonebook">
-            <Form></Form>
+      <Form></Form>
       <Section title="Contacts">
         <Filter />
-        {isLoading ? <Loader size='40px'/> : <ContactsList contacts={data}>Contacts</ContactsList>
-}
+        {isLoading ? (
+          <Loader size="40px" />
+        ) : (
+          <ContactsList contacts={data}>Contacts</ContactsList>
+        )}
       </Section>
-      <ToastContainer position="top-left" autoClose={3000} />
-    </Section>)
+      <ToastContainer position="top-left" autoClose={10000} />
+    </Section>
+  );
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,29 +3,27 @@ import { toast } from 'react-toastify';
 import { Loader } from '../Loader/Loader';
 
 import { ContactStyle, ButtonStyle } from './ContactStyle.styled';
-import { useDeleteContactMutation, useGetContactByIdQuery, useUpdateContactMutation } from '../../redux/contactsRTKQ';
+import { useDeleteContactMutation } from '../../redux/contactsRTKQ';
 
 export function Contact({ number, name, id }) {
-    const [deleteContact, {isLoading, isSuccess}] = useDeleteContactMutation();
-  // const { data: updateContactById } = useGetContactByIdQuery(id);
-  // const [updateContact, res] = useUpdateContactMutation();
-console.log(isSuccess);
-  // if(isSuccess) {toast(`Contact named ${name} deleted`)}
-
-  const handleUpdateContact = (id) => {
-    console.log(id);
+  const [deleteContact, { isLoading, isSuccess }] = useDeleteContactMutation();
+  if (isSuccess) {
+    toast(`Contact named ${name} deleted`);
   }
 
   return (
-    <ContactStyle id={id} >
-      <ButtonStyle disabled={isLoading} type="button" onClick={() => deleteContact(id)}>
+    <ContactStyle id={id}>
+      <ButtonStyle
+        disabled={isLoading}
+        type="button"
+        onClick={() => deleteContact(id)}>
         X
       </ButtonStyle>
-      {isLoading ? <Loader title="deletion..." color='red'/> : <p className='text'>{`${name}: ${number}`}</p>}
-            {/* <ButtonStyle type="button" onClick={() => handleUpdateContact(id)}>
-        E
-      </ButtonStyle> */}
-
+      {isLoading ? (
+        <Loader title="deletion..." color="red" />
+      ) : (
+        <p className="text">{`${name}: ${number}`}</p>
+      )}
     </ContactStyle>
   );
 }
