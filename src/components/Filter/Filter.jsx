fb1e65ed1a00741418403f7c1d86c9069665995b
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-05-20 19:23:17 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,19 @@
 import { Label, Input } from '../Form/FormStyle.styled';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { filter } from '../../redux/store';
 
 export function Filter() {
   const dispatch = useDispatch();
+  const filterValue = useSelector(state => state.filter);
 
   return (
     <Label>
       Find contacts by name
-      <Input onChange={e => dispatch(filter(e.target.value))} type="text" />
+      <Input
+        onChange={e => dispatch(filter(e.target.value))}
+        value={filterValue}
+        type="text"
+      />
     </Label>
   );
 }
